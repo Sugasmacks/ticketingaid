be8a623fd3ed2217c026f4809a325711f5a94abf
Interknet <interknet@yahoo.co.uk>
2018-04-08 20:02:51 +0100

    Latest

---
+++ Form2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BadlandsLSPDTicketingAid
+{
+    public partial class Form2 : Form
+    {
+        
+        public Form2()
+        {
+            InitializeComponent();
+            WindowsFormsApplication1.Form1 f1 = new WindowsFormsApplication1.Form1();
+          
+        }
+
+            private void Location_Changed(object sender, EventArgs e)
+    {
+
+    }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VersionAid_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
